02079c6535b62922ad96df71d5e37d7f89546791
Bhavika Gianey <bhavikagianey@gmail.com>
2021-07-06 11:45:44 +0530

    i2

--- server.js
+++ server.js
@@ -249,6 +249,6 @@ io.on('connection', socket => {
       });
     });
 
-      server.listen(3000, () => {
+      server.listen(process.env.PORT || 3000, () => {
         console.log("app now listening for requests on port 3000");
       });
